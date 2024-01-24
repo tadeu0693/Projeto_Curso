cb4026563f07a54a5d78b7f246c009b31a131b17
Willian Sousa <williansousa4k@gmail.com>
2024-01-24 14:06:51 -0300

    Redirecionando os produtos para o carrinho ok

--- Codigo_fonte/carrinho/script.js
+++ Codigo_fonte/carrinho/script.js
@@ -29,12 +29,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Exibir os produtos no carrinho
   carrinho.forEach(function(produto) {
-      adicionarProdutoAoCarrinho(produto);
+    adicionarProdutoAoCarrinho(produto);
   });
 
   function adicionarProdutoAoCarrinho(produto) {
       let itemCarrinho = document.createElement("li");
-      itemCarrinho.innerHTML = `<img src="${produto.imagem}" alt="${produto.nome}"> ${produto.nome} - R$${produto.preco.toFixed(0)}`;
+
+      // Verificar se a propriedade 'preco' existe antes de usá-la
+      let precoFormatado = typeof produto.preco === 'number' ? `R$${produto.preco.toFixed(0)}` : "Preço não disponível";
+
+      //itemCarrinho.innerHTML = `<img src="${produto.imagem}" alt="${produto.nome}"> ${produto.nome} - R$${produto.preco.toFixed(0)}`;
+      itemCarrinho.innerHTML = `<img src="${produto.imagem}" alt="${produto.nome}"> ${produto.nome} - ${precoFormatado}`;
 
       let removerBotao = document.createElement("button");
       removerBotao.textContent = "Remover";
@@ -54,7 +59,10 @@ document.addEventListener("DOMContentLoaded", function() {
   function calcularTotalCarrinho() {
       let total = 0;
       carrinho.forEach(function(produto) {
+        // Verificar se a propriedade 'preco' está definida
+        if (typeof produto.preco === 'number') {
           total += produto.preco;
+      }
       });
 
       totalCarrinhoElement.textContent = total.toFixed(2);
